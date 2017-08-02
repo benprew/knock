7d7105b09e1346f1fa6ce1263bf8686202830965
Ben Prew <ben@throwingbones.com>
2017-08-01 23:35:53 -0700

    Set instance var instead of trying to regenerate method

--- lib/knock/authenticable.rb
+++ lib/knock/authenticable.rb
@@ -1,8 +1,12 @@
 module Knock::Authenticable
-  def authenticate_for entity_class
-    getter_name = "current_#{entity_class.to_s.parameterize.underscore}"
-    define_current_entity_getter(entity_class, getter_name)
-    public_send(getter_name)
+  def authenticate_for(entity_class)
+    getter_name = "@current_#{entity_class.to_s.parameterize.underscore}"
+    current = begin
+      Knock::AuthToken.new(token: token).entity_for(entity_class)
+    rescue Knock.not_found_exception_class, JWT::DecodeError
+      nil
+    end
+    instance_variable_set(getter_name, current)
   end
 
   private
@@ -37,24 +41,6 @@ module Knock::Authenticable
   def token_from_request_headers
     unless request.headers['Authorization'].nil?
       request.headers['Authorization'].split.last
-    end
-  end
-
-  def define_current_entity_getter entity_class, getter_name
-    unless self.respond_to?(getter_name)
-      memoization_var_name = "@_#{getter_name}"
-      self.class.send(:define_method, getter_name) do
-        unless instance_variable_defined?(memoization_var_name)
-          current =
-            begin
-              Knock::AuthToken.new(token: token).entity_for(entity_class)
-            rescue Knock.not_found_exception_class, JWT::DecodeError
-              nil
-            end
-          instance_variable_set(memoization_var_name, current)
-        end
-        instance_variable_get(memoization_var_name)
-      end
     end
   end
 end
